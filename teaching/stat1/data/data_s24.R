dbe26381ab72cdbce205192ba14a9a7ce3fe500d
Lauren Perry, PhD <perry@csus.edu>
2026-08-19 09:14:19 -0700

    Delete teaching/stat1/data directory

--- teaching/stat1/data/data_s24.R
+++
@@ -1,4 +0,0 @@
-survey <- read.delim("https://lgpperry.github.io/teaching/stat1/data/Stat1_SurveyData_S24.csv")
-survey[sapply(survey, is.character)] <- lapply(survey[sapply(survey, is.character)], as.factor)
-print("Use the command `str(survey)` to view the variables in the data.")
-print("The command `attach(survey)` will allow you to access the variables directly.")
